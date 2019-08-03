a1647cd24cbb92789899be15fb4c482e1100f54e
Niketan E. Moon <niketanmoon@gmail.com>
2019-08-03 12:02:19 +0530

    Encoding the categorical data into numeric field and introducing dummy encoding

--- Data Preprocessing/Day3- Dealing with the categorical data/categoricaldata.py
+++ Data Preprocessing/Day3- Dealing with the categorical data/categoricaldata.py
@@ -25,4 +25,19 @@ X[:,1:3] = imputer.transform(X[:,1:3])
 #Tackling the categorical data. Encoding the categorical data into numeric dataset
 #First import the library for doing the encoding 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#first creating the object of labelencoder
+labelencoder_X = LabelEncoder()
+#Specifying the column
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+
+#Since the data can be misinterpreted as smaller than or greater than, We do dummy encoding 
+#Creating the object for onehotencoder
+onehotencoder = OneHotEncoder(categorical_features=[0]) #Specifying the column which has to be encoded
+X= onehotencoder.fit_transform(X).toarray()
+
+#Since there are only two categories doing only the label encoding of Y dataset
+# First creating the object for Y dataset of LabelEncoder
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
 
